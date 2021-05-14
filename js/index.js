177c5fd64a23663abbf43bbd51822d44e8422675
mplutarc <anda.ere@yandex.ru>
2021-05-14 14:40:23 +0300

    доработан внешний вид и функционал формы

--- js/index.js
+++ js/index.js
@@ -1,4 +1,38 @@
-function resetForm() {
-	document.getElementById('myform').reset();
-	console.log("reset all");
-}
+$(document).ready(function () {
+	$("#myform").change(function() {
+		let checkFields = [$.trim($('#name').val()),
+			$.trim($('#bdate').val()),
+			$.trim($('#email').val()),
+			$.trim($('#password').val()),
+			$.trim($('#password-confirm').val()),
+			$.trim($('#secret-question').val()),
+			$.trim($('#secret-answer').val()),
+			$.trim($('#address').val()),
+			$.trim($('#city').val()),
+			$.trim($('#phone').val())]
+		let checkbox = $('#checkbox').prop('checked');
+		let i = 0;
+		$.each(checkFields, function (index, value) {
+			if (value.length > 0) {
+				i++;
+				// console.log('value не пустое, value = ', value, ', i = ', i);
+			} else {
+				// console.log('value пустое, value = ', value);
+			}
+		})
+		console.log($('#password').val() === $('#password-confirm').val());
+		if($('#password').val() === $('#password-confirm').val()) {
+			$('.pass_confirm').removeClass('has-error');
+			console.log('пароли совпадают');
+		} else {
+			$('.pass_confirm').addClass('has-error');
+			console.log('пароли не совпадают');
+		}
+		if (i >= 10 && checkbox === true) {
+			$('#submit').prop('disabled', false);
+		} else {
+			$('#submit').prop('disabled', true);
+		}
+	});
+});
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,33 @@
-const path = require('path')
+const path = require('path');
+const {webpack} = require("webpack");
 
 module.exports = {
-	entry: {
-		main: path.resolve(__dirname, './js/index.js'),
-	},
+	entry: [
+		'./js/index.js',
+	],
 	output: {
-		path: path.resolve(__dirname, './dist'),
-		filename: '[name].bundle.js',
-	}
-}
+		filename: './bundle.js'
+	},
+	devtool: "source-map",
+	module: {
+		rules: [{
+			test: /\.js$/,
+			include: path.resolve(__dirname, 'js'),
+			use: {
+				loader: 'babel-loader',
+				options: {
+					presets: ['@babel/preset-env']
+				}
+			}
+		},
+		]
+	},
+	plugins: [
+		new webpack.ProvidePlugin({ // inject ES5 modules as global vars
+			$: 'jquery',
+			jQuery: 'jquery',
+			'window.jQuery': 'jquery',
+			Popper: ['popper.js', 'default']
+		})
+	]
+};
